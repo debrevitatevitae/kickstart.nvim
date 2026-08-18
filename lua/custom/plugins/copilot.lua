5474b611fe3361a7cf4ce1dd478ead04004eb417
debrevitatevitae <giotb92@gmail.com>
2026-08-18 17:23:22 +0200

    Change acceptance key for copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,6 @@
 do
-	vim.pack.add {'https://github.com/github/copilot.vim'}
+  vim.pack.add { 'https://github.com/github/copilot.vim' }
+  vim.g.copilot_no_tab_map = true
+
+  vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true, replace_keycodes = false })
 end
