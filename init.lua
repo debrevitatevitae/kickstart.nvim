df936f62a77e30baf6124eb46678e22f5e015f2d
debrevitatevitae <giotb92@gmail.com>
2026-08-20 10:29:37 +0200

    Show diagnostics using virtual lines

--- init.lua
+++ init.lua
@@ -194,8 +194,8 @@ do
     underline = { severity = { min = vim.diagnostic.severity.WARN } },
 
     -- Can switch between these as you prefer
-    virtual_text = true, -- Text shows up at the end of the line
-    virtual_lines = false, -- Text shows up underneath the line, with virtual lines
+    virtual_text = false, -- Text shows up at the end of the line
+    virtual_lines = { current_line = true }, -- Text shows up underneath the line, with virtual lines
 
     -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
     jump = {
